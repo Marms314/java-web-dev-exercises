0f136e462937ff3737f9ea162a849b3b4f08ba69
Marms <maryarmstrong314@gmail.com>
2020-04-19 20:56:40 -0500

    Finished part two of the exercise

---
+++ src/exercises/CalculateRectangleArea.java
@@ -0,0 +1,19 @@
+package exercises;
+import java.util.Scanner;
+
+public class CalculateRectangleArea {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter the length of the rectangle:");
+        int side1 = input.nextInt();
+
+        System.out.println("Enter the width of the rectangle:");
+        int side2 = input.nextInt();
+        input.close();
+
+        int rectangleArea = side1 * side2;
+        System.out.println(rectangleArea);
+    }
+}
