329c7af4139f8761e208343a9b3af7c1a6ceeca6
Marms <maryarmstrong314@gmail.com>
2020-04-23 21:30:31 -0500

    Completed instructions for the studio and bonus mission 3 at https://education.launchcode.org/java-web-development/chapters/control-flow-and-collections/studio.html

---
+++ src/org/launchcode/java/studios/class02CountingCharacters/CountCharacters.java
@@ -0,0 +1,28 @@
+package org.launchcode.java.studios.class02CountingCharacters;
+
+import java.util.HashMap;
+import java.util.regex.Pattern;
+
+public class CountCharacters {
+    public static void main(String[] args) {
+        String sentence = "If the product of two terms is zero then common sense says at least one of the two terms has" +
+                " to be zero to start with. So if you move all the terms over to one side, you can put the quadratics" +
+                " into a form that can be factored allowing that side of the equation to equal zero. Once you’ve done" +
+                " that, it’s pretty straightforward from there.";
+
+        HashMap<Character, Integer> charactersUsed = new HashMap<>();
+
+
+        for (Character letter : sentence.toCharArray()) {
+            if (letter.toString().matches("[a-zA-Z]")) {
+                if (charactersUsed.containsKey(letter)) {
+                    charactersUsed.put(letter, charactersUsed.get(letter) + 1);
+                } else {
+                    charactersUsed.put(letter, 1);
+                }
+            }
+        }
+
+        System.out.println(charactersUsed);
+    }
+}
